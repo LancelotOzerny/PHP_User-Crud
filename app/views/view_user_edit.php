61a27b97828f4b51bccc70ee398b9bde5d0af86f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-25 23:59:14 +0300

    Добавил представления user контроллера

---
+++ app/views/view_user_edit.php
@@ -0,0 +1,1 @@
+<h2>User Edit</h2>
---
+++ app/views/view_user_list.php
@@ -0,0 +1,83 @@
+<?php
+    $userList = [];
+?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CRUD пользователей</title>
+
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <script src="assets/js/jQuery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
+</head>
+<body>
+<div id="preloader">
+    <img src="assets/img/spinner.gif" alt="wait some time...">
+</div>
+
+<div class="container">
+    <div class="row col-12">
+        <p class="display-4 text-center py-5">CRUD (AJAX) пользователей</p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="table-list-container">
+        <table class="table table-hover table-borderless">
+            <thead>
+            <tr class="border-bottom">
+                <th scope="col">#</th>
+                <th scope="col">Логин</th>
+                <th scope="col">Почта</th>
+                <th scope="col"></th>
+            </tr>
+            </thead>
+            <tbody>
+            <?php foreach($userList as $user): ?>
+                <tr>
+                    <th><?= $user['ID'] ?></th>
+                    <td><?= $user['LOGIN'] ?></td>
+                    <td><?= $user['EMAIL'] ?></td>
+                    <td>
+                        <div class="list-inline-item d-flex justify-content-end">
+                            <a href="/user/edit/?id=<?= $user['ID'] ?>"
+                               class="btn btn-secondary btn-sm rounded-1 text-uppercase"
+                               title="Edit">
+                                Редактировать
+                            </a>
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="list-inline-item d-flex justify-content-end">
+                <a href="/user/create ?>"
+                   class="btn btn-success btn-sm rounded-1 text-uppercase"
+                   title="Create">
+                    Создать
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    $(document).ready(() => {
+        $("#preloader").remove();
+    });
+</script>
+</body>
+</html>
