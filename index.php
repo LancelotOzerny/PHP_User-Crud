ddf9995d9798461f6cf4a1fc86759669ba4acd5f
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-25 19:47:42 +0300

    Добавил bootstrap, jquery, стили сброса и картинку спиннера

--- index.php
+++ index.php
@@ -6,8 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>CRUD пользователей</title>
+
+    <link rel="stylesheet" href="assets/css/reset.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <script src="assets/js/jQuery.js"></script>
+    <script src="assets/js/bootstrap.js"></script>
 </head>
 <body>
-
+    <div class="preloader">
+        <img src="assets/img/spinner.gif" alt="wait some time...">
+    </div>
 </body>
 </html>
