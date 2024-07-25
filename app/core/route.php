0b3547f768c679ac7ebc293e6b6c59bc4369b50a
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-26 00:15:18 +0300

    Реализовал подключение моделей

--- app/core/route.php
+++ app/core/route.php
@@ -22,15 +22,25 @@ class Route
         }
 
         global $HOME;
+        $model_path = "$HOME/app/models/model_$controller_name.php";
+        $model_path = str_replace('\\', '/', $model_path);
+
         $controller_name = "controller_$controller_name";
         $controller_path = "$HOME/app/controllers/$controller_name.php";
+        $controller_path = str_replace('\\', '/', $controller_path);
+
         $controller_action = "action_$controller_action";
 
         if (file_exists($controller_path))
         {
             include $controller_path;
-            $controller = new $controller_name();
 
+            if (file_exists($model_path))
+            {
+                include $model_path;
+            }
+
+            $controller = new $controller_name();
             if (method_exists($controller, $controller_action))
             {
                 $controller->$controller_action();
