5aa9747f6a4ab16d7adfc400cb92fcf2d1bbcf43
Max Belyakov <lancelot.ozernuy@gmail.com>
2024-07-26 00:03:00 +0300

    Реализовал подключение представлений

--- app/core/view.php
+++ app/core/view.php
@@ -3,8 +3,16 @@ namespace App\Core;
 
 class View
 {
-    public function generate(string $view, string $template, array $data = [])
+    public function generate(string $view, string $template, array $data = []) : void
     {
+        global $HOME;
+        $viewPath = "$HOME/app/views/view_$view.php";
+        if (file_exists($viewPath))
+        {
+            include $viewPath;
+            return;
+        }
 
+        echo "View $view is not founded!";
     }
 }
